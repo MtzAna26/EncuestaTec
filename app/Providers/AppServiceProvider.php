b68fcd3bd06daadd6cf690fb1d6c70dfde53bedb
Ana Maria Mtz <100534348+MtzAna26@users.noreply.github.com>
2024-06-20 13:09:53 -0600

    Cambios 3

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Laravel\Telescope\TelescopeApplicationServiceProvider;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,9 +12,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        if (class_exists(TelescopeApplicationServiceProvider::class)) {
-            $this->app->register(TelescopeServiceProvider::class);
-        }
+        //
     }
 
     /**
